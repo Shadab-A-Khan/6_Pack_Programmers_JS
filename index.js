c73ae7dfea40dd71262904475c8e2b4a7d37e4cd
Shadab Ahmad Khan <78543910+Shadab-A-Khan@users.noreply.github.com>
2023-10-08 20:58:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ console.log(s.substr(3,4)); //start from 3 and gives length 4
 
 
 //DOM
-var str  = document.getElementById("myHeading").innerText=s;
+var str = document.getElementById("myHeading").innerText=s;
 //or
 var str  = document.getElementById("myHeading");
 str.style.backgroundColor="red";
@@ -133,15 +133,15 @@ while(j<10){
 // ____________________________________________________________________
 // ARRAY
 
-let arr = [12, ,33,44,10];
+let arr0 = [12, ,33,44,10];
 let arr1 = [12,"Khan"];
 let arr2 = ["Shadab", "Khan"];
 
-console.log(arr);
+console.log(arr0);
 console.log(arr1);
 console.log(arr2);
 
-console.log(arr[3]);
+console.log(arr0[3]);
 console.log(arr1[1]);
 console.log(arr2[0]);
 
@@ -174,7 +174,8 @@ arr3.unshift(44);
 console.log(arr3);
 
 
-
+// //To Array type Case
+// const arr = Array.from(arr);
 
 
 // splice : spit out a part of the array and update the same array
@@ -407,3 +408,315 @@ let a = 20;
 // console.log(b); //it will give - Uncaught ReferenceError: b is not defined
 console.log(a);
 console.log(c);
+
+
+
+
+
+
+
+
+
+
+//____________________________________________________________
+// DOM Manipulation
+
+// It is used to manipulate the HTML texts
+// console.log(document.boyd); // to get the body of HTML
+// console.log(document.head); // to get the head of HTML
+// console.log(document.documentElement); // to get complete HTML
+
+console.log(document.body.firstChild);         // #text
+console.log(document.body.firstElementChild);  // <h1>Hi!</h1>
+console.log(document.body.children); //LCollection(6) [h1, h1, h1, h1, script, script]
+
+// TO get some tag by its id
+// Two ways , both does the same job
+console.log(how_id);                            //<h1 id="how_id">how</h1>
+console.log(document.getElementById("how_id")); //<h1 id="how_id">how</h1>
+
+
+//getElementsByClassName
+console.log(document.getElementsByClassName("you_class")); //HTMLCollection [h1.you_class]
+
+// if we pass two div same class 
+// we will get two HTMLCollections(2)
+// like  ::   HTMLCollection(2) [h1.you_class, h1.you_class]
+
+
+//to get 1 out of them
+const thisOne = document.getElementsByClassName("you_class");
+console.log(thisOne[0]);  // <h1 class="you_class">you</h1>
+console.log(thisOne[1]);  // <h1 class="you_class">doing</h1>
+
+// getElementsByName
+console.log(document.getElementsByName("email"));   //NodeList [input]
+const temp = document.getElementsByName("email");
+console.log(temp[0]);  //<input type="text" name="email"></input>
+
+//getElementsByTagName
+const this_one = document.getElementsByTagName("span");
+console.log(this_one);
+this_one[0].textContent="New_content"; // update the span content
+console.log(this_one);
+this_one[0].innerText="Again"; // update the span content
+
+//innerHTML : to write HTML from JS using DOM
+this_one[0].innerHTML=`<span class="myClass">Nice One </span>`; // update the span content
+
+
+// style ::  TO APPLY STYLE or TO APPLY THE CSS TO THE HTML
+this_one[0].style.backgroundColor="green"
+this_one[0].style.font="100 1.5rem 'Roboto' "
+
+
+const myBtn = document.getElementById("myBtn");
+myBtn.style.background="blue";
+myBtn.style.color="#fff";
+myBtn.style.border="#none";
+myBtn.style.padding="#2vmax 4vmax";
+myBtn.style.cursor="pointer";
+
+
+//getAttribute
+console.log(myBtn.getAttribute("id"));
+console.log(myBtn.getAttribute("class"));
+
+//setAttribute
+myBtn.setAttribute("class","newClass");
+
+myBtn.onclick = bringMeHeading;
+
+// //Create New Element like   <h1> Hii! </h1>   using DOM manipulation
+// const h2 = document.createElement("h2");
+
+// //append at the bottom 
+// document.body.append(h2);
+
+////prepend at the top 
+// document.body.prepend(h2);
+// h2.innerText="What is up? , ";
+
+////createTextNode
+// const myText = document.createTextNode(" Hey! this is a sample text");
+// h2.append(myText);
+
+ //Make the change when called , ONLY
+//to run the DOM at an specific event only
+
+
+function bringMeHeading(){
+    const h2 = document.createElement("h2");
+    h2.innerText="What is up? ,  this is a sample text ";
+    document.body.prepend(h2);    
+}
+// bringMeHeading(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//_____________________________________________________
+
+//#7 ADVANCE array methods in javascript, JavaScript
+
+// Arrow Function
+
+
+// it is similar to the anonymus function
+// like
+
+let func_ = function(a,b){
+    console.log(a+b);
+}
+func_(5,4);  //9
+
+// removing funcion and adding => changes the anonymys function to 
+//arrow Function
+let func_1 = (a,b) => {
+    console.log(a+b);
+}
+func_1(5,4); //9
+
+//Arrays default syntex has Arrow funcion, so we will use 
+// arrow function only
+
+const arr = [2, 4, 6, 8, 10];
+console.log(arr);
+
+
+//1st ways
+const myFunc2=(value,index)=>{
+    if(value==6){
+        console.log("Yes! 6 exists")
+    }
+};
+arr.find(myFunc2);
+
+
+// arrow funcitons cannot be accessed before declear 
+// means first create the func and then call it in the later lines
+// at the same time , anonymus funciton can call it
+
+// it can also be writen as 
+
+// or
+// Find Method
+//2nd ways
+arr.find((value,index)=>{
+    if(value==6){
+        console.log("Yes! 6 exists")
+    }
+});
+
+
+
+// To print all the vlaues of the array
+arr.find((value,index)=>{
+  console.log(value);
+});
+
+//check value
+arr.find((value,index)=>{
+    if(value===8){
+        console.log(value + " its Eight");
+    } 
+});
+
+
+//print all the values greater the 4
+console.log(" Print all the values Greater then 4")
+arr.find((value,index)=>{
+    if(value>4){
+        console.log(value);
+    } 
+});
+
+
+// const returns just first and only value
+const result = arr.find((value,index)=>{
+    if(value>4){
+        // console.log(value);
+        return value;
+    } 
+});
+console.log(result); // 6
+
+// filter : to get all the values we use filter
+const result_filter = arr.filter((value, index)=>{
+    if(value>4){
+        return value;
+    }
+});
+
+console.log(result_filter);  //(3) [6, 8, 10]
+
+
+//arr.find // find returns single values  : used to get single value
+//arr.filter // filter returs the value as array  : use to get the multiple value from the array
+
+console.log("array.every Method");
+// every Method :: arr.every
+// it returns the boolean value
+// it checks on all/every element
+const result_every = arr.every((value,index)=>{
+    // return value<4; // false
+    return value<20 // true : if all the values are smalelr then 20 in arr
+});
+
+console.log(result_every);
+
+
+
+console.log("array.some Method");
+// some Method :: arr.some
+// it returns the boolean value
+// it checks  if anyelement satify the coditon
+const result_some = arr.some((value,index)=>{
+    // return value<4; // false
+    return value<4 // true : if all the values are smalelr then 20 in arr
+});
+
+console.log(result_some);
+
+
+// For of loop 
+// for each does not return any thing
+// it does not change the actual array
+console.log("_______For of loop_______");
+for(const element of arr){
+    console.log(element);
+}
+
+
+console.log("_______update the value fo the array and print_______");
+for(let value of arr){
+    value+=3;
+    console.log(value);
+}
+
+
+console.log("For Each loop");
+arr.forEach((value,index)=>{
+    console.log(value);
+    console.log(index);
+})
+
+console.log("Total Sum using For each loop");
+let totalSum_1=0;
+arr.forEach((value,index)=>{
+    totalSum_1+=value;
+});
+console.log(totalSum_1);
+
+
+// let totalSum_2=0;
+// arr.forEach((value,index)=>{
+//     value+=20;
+//     console.group(value);
+// });
+// console.log(totalSum_2);
+
+
+const val1 = arr.forEach((value,index)=>{
+    return value;
+});
+console.log(val1);
+
+
+
+console.log("arr.map funcion");
+// it will return the new array
+// it also do not change anything in actual array
+
+const ress = arr.map((value,index)=>{
+     return value+30;
+});
+console.log(ress);  //  (5) [32, 34, 36, 38, 40]
+console.log(arr);   //  (5) [2, 4, 6, 8, 10]
+
+
+console.log("arr.reduce :: Method");
+// reduce sum up all the value of the array
+// total = zero index value
+// value = first index value
+// index start from 1
+const resul_t = arr.reduce((total,value,index,newArr)=>{
+    console.log(value);
+    console.log(index);
+   return total;
+})
+console.log(resul_t);
