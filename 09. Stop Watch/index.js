e50490f2fc7558da1bf4baac208e08f1ce025873
Shadab Ahmad Khan <78543910+Shadab-A-Khan@users.noreply.github.com>
2023-10-30 22:35:29 +0530

    Update index.js

--- 09. Stop Watch/index.js
+++ 09. Stop Watch/index.js
@@ -6,21 +6,24 @@ let minute = 0;
 let hour = 0;
 let intervalId;
 const startStopWatch = () => {
+  
   intervalId = setInterval(() => {
     second++;
-    const time = `${String(hour).padStart(2, "0")}:${String(minute).padStart(
-      2,
-      "0"
-    )}:${String(second).padStart(2, "0")}`;
+    const time =`${String(hour).padStart(2,"0")}:
+                 ${String(minute).padStart(2,"0")}:
+                 ${String(second).padStart(2,"0")}`;
+
     timeElement.innerText = time;
-    if (second == 60) {
+
+    if(second == 60){
       second = 0;
       minute += 1;
-      if (minute == 60) {
+      if(minute == 60) {
         minute = 0;
         hour += 1;
       }
     }
+
   }, 1000);
 };
 
